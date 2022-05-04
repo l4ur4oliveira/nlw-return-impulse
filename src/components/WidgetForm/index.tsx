61044b33fa9bbda93ad26346f380037763036077
Laura de Oliveira <l4ur4.88@gmail.com>
2022-05-04 20:34:31 -0300

    Aula 2 - Separa componentes do box do widget

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+import { FeedbackType } from "./Steps/FeedbackType";
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      soucer: bugImageUrl,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      soucer: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      soucer: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  }
+}
+
+export type FeedbackTypes = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackTypes | null>(null)
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+
+      {!feedbackType ? (
+        <FeedbackType onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <p>Hello World</p>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela <a className="underline underline-offset-2" href="#">Rocketseat</a>
+      </footer>
+    </div>
+  )
+}
