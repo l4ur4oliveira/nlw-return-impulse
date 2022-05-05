d0c7aaf62d7ffb0261d37a3eb87f43ab0cadad95
Laura de Oliveira <l4ur4.88@gmail.com>
2022-05-05 20:54:11 -0300

    Aula 3 - Configura envio de email com Mailtrap

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "21c0239dbd64fd",
+    pass: "bacd6e44a38545"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot} = req.body;
@@ -14,6 +24,16 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot
     }
+  });
+
+  transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Lau Oliveira <oi@laura.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+    ].join('\n')
   });
 
   return res.status(201).json({data: feedback});
