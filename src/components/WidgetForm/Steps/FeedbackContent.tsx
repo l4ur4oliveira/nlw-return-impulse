b471d290c433f45c6df3fb81a6442a7530416c54
Laura de Oliveira <l4ur4.88@gmail.com>
2022-05-04 22:52:26 -0300

    Aula 2 - Isola o botão de screenshot em um componente

--- src/components/WidgetForm/Steps/FeedbackContent.tsx
+++ src/components/WidgetForm/Steps/FeedbackContent.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackTypes, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentProps {
   feedbackType: FeedbackTypes;
@@ -11,6 +13,8 @@ export function FeedbackContent({
   feedbackType,
   onFeedbackRestart
 }: FeedbackContentProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   return (
@@ -37,12 +41,11 @@ export function FeedbackContent({
         />
 
         <footer className="flex gap-2 mt-2">
-          <button
-            type="button"
-            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-          >
-            <Camera className="w-6 h-6" />
-          </button>
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
+
           <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
