1993830d51744af24dfa76e6f8887e27f83c038c
Laura de Oliveira <l4ur4.88@gmail.com>
2022-05-05 20:42:40 -0300

    Aula 3 - Adiciona rota para criar feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,24 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.send('Hellow');
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot} = req.body;
+
+  const feedback = await prisma.feedbacks.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  });
+
+  return res.status(201).json({data: feedback});
 });
 
 app.listen(3333, () => {
   console.log('Server running...');
-  
 });
