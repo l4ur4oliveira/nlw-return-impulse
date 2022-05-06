46b06965ca6c0d59402b2dd37ac50c47c90e4a96
Laura de Oliveira <l4ur4.88@gmail.com>
2022-05-05 21:24:05 -0300

    Aula 3 - Aplica inversão de dependência (SOLID) na criação do feedback

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import nodemailer from 'nodemailer';
-import { prisma } from "./prisma";
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router();
 
@@ -16,23 +17,26 @@ const transport = nodemailer.createTransport({
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot} = req.body;
 
-  const feedback = await prisma.feedbacks.create({
-    data: {
-      type,
-      comment,
-      screenshot
-    }
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository
+  );
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot
   });
 
-  await transport.sendMail({
-    from: 'Equipe Feedget <oi@feedget.com>',
-    to: 'Lau Oliveira <oi@laura.com>',
-    subject: 'Novo feedback',
-    html: [
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-    ].join('\n')
-  });
+  // await transport.sendMail({
+  //   from: 'Equipe Feedget <oi@feedget.com>',
+  //   to: 'Lau Oliveira <oi@laura.com>',
+  //   subject: 'Novo feedback',
+  //   html: [
+  //     `<p>Tipo do feedback: ${type}</p>`,
+  //     `<p>Comentário: ${comment}</p>`,
+  //   ].join('\n')
+  // });
 
-  return res.status(201).json({data: feedback});
+  return res.status(201).send();
 });
